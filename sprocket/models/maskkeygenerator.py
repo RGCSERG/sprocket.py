d2052c3841610843e29cab210811731f7d72bff4
RGCSERG <sergentrufus@gmail.com>
2023-10-12 16:29:06 +0100

    changed __all__

--- sprocket/models/maskkeygenerator.py
+++ sprocket/models/maskkeygenerator.py
@@ -16,6 +16,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE."""
 
+from typing import Final, List
+
+
+__all__: Final[List[str]] = ["CustomRandomGenerator"]
+
 
 class CustomRandomGenerator:
     def random(self):
