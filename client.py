c318ced42285768a746a48264dd7ec7764ace2c6
RGCSERG <sergentrufus@gmail.com>
2023-10-18 11:09:23 +0100

    testing

--- client.py
+++ client.py
@@ -9,4 +9,5 @@ if __name__ == "__main__":
         "wassup using threading now frrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
     )
     client.ping()
+    time.sleep(30)
     client.close()
