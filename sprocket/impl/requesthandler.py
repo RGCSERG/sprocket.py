5a54ab2c6164575ab5b55bf63058611dae516d03
RGCSERG <sergentrufus@gmail.com>
2023-11-29 21:17:58 +0000

    requesthandler changes

--- sprocket/impl/requesthandler.py
+++ sprocket/impl/requesthandler.py
@@ -17,59 +17,26 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE."""
 
 from http import HTTPStatus
-from typing import Final, List, Union  # Used for type annotations and decloration.
+from loguru import logger
+from typing import (
+    Final,
+    List,
+    Literal,
+)  # Used for type annotations and decloration.
 
 
 __all__: Final[List[str]] = ["HTTPRequestHandler"]
 
 
 class HTTPRequestHandler:
-    def __init__(self) -> None:
+    def __init__(self, WS_ENDPOINT: str) -> None:
+        self.WS_ENDPOINT = WS_ENDPOINT
+
         self.request_data: str = ""
         self.method: str = ""
         self.target: str = ""
         self.headers: dict = {}
         self.http_version: str = ""
-
-    def process_request(self, request_data: str) -> Union[str, None]:
-        # Parse the HTTP request
-
-        self.request_data = request_data
-
-        request = self._parse_request(self.request_data)
-
-        if not request:
-            response_body = "<html><body><h1>Default bad response</h1></body></html>"
-            response_headers = f"HTTP/1.1 {HTTPStatus.BAD_REQUEST}\r\nContent-Length: {len(response_body)}\r\n\r\n"
-            return response_headers + response_body
-
-        method, target, headers, http_version = request
-
-        self.method = method
-        self.target = target
-        self.headers = headers
-        self.http_version = http_version
-
-        response_body = "<html><body><h1>Default response</h1></body></html>"
-        response_headers = (
-            f"HTTP/1.1 {HTTPStatus.OK}\r\nContent-Length: {len(response_body)}\r\n\r\n"
-        )
-        return response_headers + response_body
-
-    def _parse_request(self, request_data: str) -> tuple[str, str, dict, str]:
-        # Split the request data into lines to parse individual components
-        request_lines: list[str] = request_data.split("\r\n")
-
-        if not request_lines:
-            return None
-
-        # Parse the request line to extract method, target, and HTTP version
-        method, target, http_version = self._parse_request_line(request_lines[0])
-
-        # Parse headers
-        headers: dict = self._parse_headers(request_lines[1:])
-
-        return method, target, headers, http_version
 
     def _parse_request_line(self, request_line: list[str]) -> tuple[str, str, str]:
         method: str
@@ -86,5 +53,81 @@ class HTTPRequestHandler:
                 header_name, header_value = line.split(":", 1)
                 header_name = header_name.strip()
                 header_value = header_value.strip()
-                headers[header_name.lower()] = header_value
+                headers[
+                    header_name.lower()  # Headers should be set to lower case, for all mapping and classification rules.
+                ] = header_value
         return headers
+
+    def _is_valid_ws_handshake_request(
+        self, method: str, target: str, headers: dict, http_version: str
+    ) -> bool:
+        logger.debug("Handling WebSocket handshake request.")
+
+        if method != "GET":
+            return False
+
+        if target != self.WS_ENDPOINT:
+            return False
+
+        try:
+            _, version = http_version.split("/")
+            http_version: float = float(version)
+        except (ValueError, IndexError):
+            return False  # Invalid HTTP version format.
+
+        if http_version < 1.1:
+            return False
+
+        headers_valid: bool = (
+            headers.get("upgrade", "") == "websocket"
+            and headers.get("connection", "") == "Upgrade"
+            and "sec-websocket-key" in headers
+        )
+        return headers_valid
+
+    def _parse_request(self, request_data: str) -> tuple[str, str, dict, str]:
+        # Split the request data into lines to parse individual components
+        request_lines: list[str] = request_data.split("\r\n")
+
+        if not request_lines:
+            return None
+
+        # Parse the request line to extract method, target, and HTTP version
+        method, target, http_version = self._parse_request_line(request_lines[0])
+
+        # Parse headers
+        headers: dict = self._parse_headers(request_lines[1:])
+
+        return method, target, headers, http_version
+
+    def process_request(
+        self, request_data: str
+    ) -> tuple[str, Literal[False]] | tuple[dict, Literal[True]]:
+        response_body = "<html><body><h1>Default bad response</h1></body></html>"
+        response_headers = f"HTTP/1.1 {HTTPStatus.BAD_REQUEST}\r\nContent-Length: {len(response_body)}\r\n\r\n"
+
+        full_response = response_headers + response_body
+
+        self.request_data = request_data
+
+        request = self._parse_request(self.request_data)
+
+        method, target, headers, http_version = request
+
+        self.method = method
+        self.target = target
+        self.headers = headers
+        self.http_version = http_version
+
+        if not request:
+            return full_response, False
+
+        if not self._is_valid_ws_handshake_request(
+            method=method, target=target, headers=headers, http_version=http_version
+        ):
+            logger.warning("Handshake invalid.")
+            return full_response, False
+
+        logger.success("Handshake valid.")
+
+        return headers, True
