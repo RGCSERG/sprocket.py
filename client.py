ae3e7d50fc3d82200d39c0bc7f14dfb58e900ef4
RGCSERG <sergentrufus@gmail.com>
2023-11-30 17:04:03 +0000

    Functionality overhaul = using more robust system

--- client.py
+++ client.py
@@ -1,49 +1,16 @@
-import time
-
-from loguru import logger
-from pydantic import BaseModel
 from sprocket import ClientSocketImpl
 
-client = ClientSocketImpl()
-messages = []
+messages: list = []
+io = ClientSocketImpl()
 
 
-# def stuff_will_happen(args, kwargs):
-#     client.send_websocket_message(message="aint no way")
-#     client.send_websocket_message(message="aint no way2")
-#     client.send_websocket_message(message="aint no way3" event="send_message")
-#     client.close()
-class Message(BaseModel):
-    owner_id: int
-    message: str
-    room_name: str
+def on_recieve(message):
+    messages.append(message["message"])
 
 
-def prints(args, kwargs):
-    dictt = eval(args.replace("'", '"'))
-    message = Message(**dictt)
-    messages.append(message.message)
-    print(messages)
+io.on("recieve_message", on_recieve)
 
+io.start()
 
-# client.on("send_message", stuff_will_happen)
-client.on("recieved_message", prints)
-
-
-if __name__ == "__main__":
-    client.start()
-    client.send_websocket_message(message="room1", event="join_room")
-
-while True:
-    inn = int(input("1 or 0: "))
-    if inn:
-        message = input("what the heeeeeeeeeeeeeeel: ")
-        messages.append(message)
-        message = {
-            "owner_id": client.ID,
-            "message": message,
-            "room_name": "room1",
-        }
-        client.send_websocket_message(message=str(message), event="send_message")
-    else:
-        client.close()
+io.emit("join_room", "1234")
+io.emit("send_message", {"message": "big money", "room_ID": "1234"})
--- sprocket.py
+++ sprocket.py
@@ -1,50 +1,24 @@
+from sprocket import ServerSocketImpl
 import socket
-import time
 from loguru import logger
 
-from pydantic import BaseModel
-from sprocket import ServerSocketImpl
-
-
-server = ServerSocketImpl()
-
-
-def deal_with_stuff(args, kwargs):
-    server.join_room(args, kwargs)
-
-
-class Message(BaseModel):
-    owner_id: int
-    message: str
-    room_name: str
-
-
-def send(args, kwargs):
-    message = Message(**eval(args.replace("'", '"')))
-
-    server.broadcast_to_room(
-        message=str(dict(message)),
-        client_socket=kwargs,
-        event="recieved_message",
-        room_name=message.room_name,
-    )
+io = ServerSocketImpl()
 
 
 def main(socket: socket):
-    logger.success(f"socket {socket} connected")
+    def on_join(room_name: str):
+        logger.success(room_name)
+        io.join_room(room_name=room_name, socket=socket)
+
+    def on_send(message):
+        logger.success(message)
+        io.to(room_name=message["room_ID"]).emit("recieve_message", payload=message)
+
+    io.on("join_room", on_join)
+    io.on("send_message", on_send)
 
 
-server.on("connect", main)
-server.on("join_room", deal_with_stuff)
-server.on("send_message", send)
+io.on("connection", main)
 
 if __name__ == "__main__":
-    server.start()
-
-# class SocketConnected:
-#     def __init__(self, socket:socket) -> None:
-#         self.socket = socket
-#         self.socket.on?
-
-#     def start(self):
-"""Maybe implement this approach?"""
+    io.start()
