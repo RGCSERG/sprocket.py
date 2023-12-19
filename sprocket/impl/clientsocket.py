7f0d02d9768dae7b9d0e102767ce1c46833779cd
RGCSERG <sergentrufus@gmail.com>
2023-12-19 11:36:04 +0000

    -+ name change

--- sprocket/impl/clientsocket.py
+++ sprocket/impl/clientsocket.py
@@ -17,16 +17,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE."""
 
 
-import random, threading, json
+import threading, json
 from typing import Callable, Final, List, Optional
 from loguru import logger
 
 from .clientsocketbase import *
 
-__all__: Final[List[str]] = ["ClientSocketImpl"]
+__all__: Final[List[str]] = ["ClientSocket"]
 
 
-class ClientSocketImpl(ClientSocketBaseImpl):  #  comments + sort layout
+class ClientSocket(ClientSocketBaseImpl):  #  comments + sort layout
     def __init__(
         self,
         HOST: Optional[str] = "localhost",
@@ -43,52 +43,79 @@ class ClientSocketImpl(ClientSocketBaseImpl):  #  comments + sort layout
             MAX_FRAME_SIZE=MAX_FRAME_SIZE,
         )
 
-        self.ID = random.randint(
-            1000000, 9999999
-        )  # maybe implement some decent id system
-
     def start(self) -> None:
         try:
-            self._client_socket.connect((self._HOST, self._PORT))
+            self._client_socket.connect(
+                (self._HOST, self._PORT)
+            )  # Connect the Client Socket to the server.
 
-            logger.debug(f"Connected to {self._HOST}:{self._PORT}")
+            logger.success(
+                f"Connected to {self._HOST}:{self._PORT}"
+            )  # Log a successful connection.
 
-            if self._perform_websocket_handshake():
-                logger.success("WebSocket handshake successful")
+            if self._perform_websocket_handshake():  # Perform a WebSocket handshake.
+                logger.success(
+                    "WebSocket handshake successful"
+                )  # Log a successfull handshake.
 
-                self._socket_open = True
-                listen_thread = threading.Thread(target=self._listen_for_messages)
-                listen_thread.start()
+                self._socket_open = True  # Set the socket to open.
+
+                listen_thread = threading.Thread(
+                    target=self._listen_for_messages
+                )  # Open a listening thread.
+                listen_thread.start()  # Start the listening thread.
 
             else:
-                logger.warning("WebSocket handshake failed")
-        except ConnectionRefusedError:
-            logger.warning("Connection to server actively refused")
+                logger.warning(
+                    "WebSocket handshake failed"
+                )  # Log an unsuccessful handshake.
+
+        except ConnectionRefusedError:  # Catch a connection error.
+            logger.warning("Connection to server actively refused")  # Log the error.
+
+        return
 
     def close(self) -> None:
         logger.warning("Sending Close Frame")
-        if self._socket_open:
-            self._send_websocket_message(opcode=self._frame_types.close)
+
+        if self._socket_open:  # If the socket is not already closed.
+            self._send_websocket_message(
+                opcode=self._frame_types.close
+            )  # Send a close frame.
+
+        return
 
     def ping(self) -> None:
-        """
-        Send a Ping frame to the server.
-        """
-        if self._socket_open:
+        if self._socket_open:  # if the socket is open.
             logger.debug("Activating Ping")
-            self._send_websocket_message(opcode=self._frame_types.ping)
+
+            self._send_websocket_message(
+                opcode=self.control_frame_types.ping
+            )  # Send a ping frame to the client socket.
+
+        return
 
     def on(self, event: str, handler: Callable) -> None:
-        if event not in self._event_handlers:
-            self._event_handlers[event] = []
-        self._event_handlers[event].append(handler)
+        if event not in self._event_handlers:  # If event not found.
+            self._event_handlers[
+                event
+            ] = []  # Append it to the _event_handlers dictionary.
+        self._event_handlers[event].append(
+            handler
+        )  # Then append the provided handler to the dictionary entry.
+
+        return
 
     def emit(self, event: str, payload: (str | bytes | dict | None)) -> None:
-        json_data: dict = {event: payload}
+        json_data: dict = {event: payload}  # Set up the json_data.
 
-        payload: str = json.dumps(json_data)
+        payload: str = json.dumps(json_data)  # Dump the json_data into str format.
 
-        frames = self._frame_encoder.encode_payload_to_frames(payload=payload)
+        frames = self._frame_encoder.encode_payload_to_frames(
+            payload=payload
+        )  # Encode the payload into WebSocket frames.
 
-        for frame in frames:
-            self._client_socket.send(frame)
+        for frame in frames:  # For each frame created.
+            self._client_socket.send(frame)  # Send it to the server.
+
+        return
